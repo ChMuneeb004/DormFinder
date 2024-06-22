a1659434916625ce6ceace1f3b2a6b1ade64aa2e
ChMuneeb004 <102245797+ChMuneeb004@users.noreply.github.com>
2024-06-23 01:28:29 +0500

    Side Bar

--- client/src/dashbordComp/Sidebar.jsx
+++ client/src/dashbordComp/Sidebar.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import '../css_folder/sidebar.css';
-import ProfileImg from '../media/profile.png';
-import { Navbar, Nav, NavDropdown, NavbarText } from 'react-bootstrap';
+import ProfileImg from '../media/Profile.jpg';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
@@ -39,11 +39,13 @@ function Sidebar({ onProfileClick }) {
         };
     }, []);
 
+    const profileImageClasses = "img-thumbnail rounded-circle mx-auto d-block";
+
     if (isMobile) {
         return (
             <Navbar expand="lg" className="mobile-navbar p-0">
-                <Navbar.Brand className=" p-2 d-flex align-items-center" onClick={onProfileClick} style={{ cursor: 'pointer' }}>
-                    <img src={ProfileImg} alt="profile" className="profiles-picture" />
+                <Navbar.Brand className="p-2 d-flex align-items-center" onClick={onProfileClick} style={{ cursor: 'pointer' }}>
+                    <img src={ProfileImg} alt="profile" className={profileImageClasses} style={{ width: '100px', height: '100px', objectFit: 'cover' }} />
                     <div className="profile-info m-2">
                         <h3 className='username text-black'>{username}</h3>
                         <p className='role'>Owner</p>
@@ -62,7 +64,7 @@ function Sidebar({ onProfileClick }) {
                         Dorm Finder
                     </span>
                 </div>
-                <Navbar.Toggle className=" m-2 btn btn-primary text-white" aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle className="m-2 btn btn-primary text-white" aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Link as={Link} to="/Dashboard/Home"><i className='fa fa-home'></i>Home</Nav.Link>
@@ -82,8 +84,8 @@ function Sidebar({ onProfileClick }) {
     }
 
     return (
-        <div className="side">
-            <div className=' text-center align-content-center' style={{ marginBottom: '15px', padding: '4px', borderWidth: '1px', border: '1px', borderStyle: 'groove', borderRadius:'12px' , display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div className="bg p-3 d-flex flex-column" style={{ minHeight: '100vh' }}>
+            <div className="text-center mb-4 p-2 border rounded d-flex align-items-center justify-content-center" style={{ borderWidth: '1px', borderStyle: 'groove' }}>
                 <img
                     src="Logo.png"
                     width="25"
@@ -92,33 +94,50 @@ function Sidebar({ onProfileClick }) {
                     alt="Logo"
                     style={{ verticalAlign: 'middle' }}
                 />
-                <span style={{ display: 'inline-block', fontSize: '22px', marginLeft: '8px' }}>
+                <span className="ml-2" style={{ fontSize: '22px' }}>
                     Dorm Finder
                 </span>
             </div>
 
             <div
-                className="profiles"
+                className="text-center mb-4"
                 onClick={onProfileClick}
                 role="button"
                 tabIndex="0"
                 style={{ cursor: 'pointer' }}
             >
-                <img src={ProfileImg} alt="profile" className="profiles-picture" />
-                <h3 className='text-body-emphasis username'>{username}</h3>
-                <p className='role'>Owner</p>
+                <img src={ProfileImg} alt="profile" className={profileImageClasses} style={{ width: '100px', height: '100px', objectFit: 'cover' }} />
+                <h3 className="font-weight-bold">{username}</h3>
+                <p className="text-warning">Owner</p>
             </div>
-            <Nav className="Navv flex-column flex-sm-fill flex-fill">
-                <Nav.Link href="/Dashboard/Home" className="nav-link"><i className='fa fa-home'></i>Home</Nav.Link>
-                <Nav.Link href="/bookings" className="nav-link"><i className='fa fa-bookmark'></i>Booking</Nav.Link>
-                <Nav.Link href="/hostel" className="nav-link"><i className='fa fa-building'></i>Hostel</Nav.Link>
-                <Nav.Link href="/payment" className="nav-link"><i className='fa fa-dollar'></i>Payment</Nav.Link>
-                <Nav.Link href="/report" className="nav-link"><i className='fa fa-file-text'></i>Report</Nav.Link>
+
+            <Nav className="flex-column">
+                <Nav.Link href="/Dashboard/Home" className="d-flex align-items-center mb-3">
+                    <i className="fa fa-home mr-2"></i>
+                    Home
+                </Nav.Link>
+                <Nav.Link href="/bookings" className="d-flex align-items-center mb-3">
+                    <i className="fa fa-bookmark mr-2"></i>
+                    Booking
+                </Nav.Link>
+                <Nav.Link href="/hostel" className="d-flex align-items-center mb-3">
+                    <i className="fa fa-building mr-2"></i>
+                    Hostel
+                </Nav.Link>
+                <Nav.Link href="/payment" className="d-flex align-items-center mb-3">
+                    <i className="fa fa-dollar mr-2"></i>
+                    Payment
+                </Nav.Link>
+                <Nav.Link href="/report" className="d-flex align-items-center mb-3">
+                    <i className="fa fa-file-text mr-2"></i>
+                    Report
+                </Nav.Link>
             </Nav>
-            <div className="logout-button">
+
+            <div className="mt-auto text-center">
                 <Link to="/login">
-                    <button className="btn btn-danger">
-                        <i className="fa fa-sign-out"></i> Logout
+                    <button className="btn btn-danger btn-block">
+                        <i className="fa fa-sign-out mr-2"></i> Logout
                     </button>
                 </Link>
             </div>
