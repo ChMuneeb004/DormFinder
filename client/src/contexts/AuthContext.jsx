b1d933af74d5c85327aa0d74d1fc27d8cf9b627e
ChMuneeb004 <102245797+ChMuneeb004@users.noreply.github.com>
2024-07-06 03:29:30 +0500

    OK

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }) => {
     const login = async (email, password) => {
         try {
             // debugger;
-            const response = await axios.post('http://localhost:3001/login', { email, password });
+            const response = await axios.post('https://dorm-finder.vercel.app/login', { email, password });
             if (response.data.message === 'success') {
                 const { token, userType } = response.data;
                 localStorage.setItem('token', token);
